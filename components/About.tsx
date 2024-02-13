2fc0b659b6e248df870de8fb19680ea7ce853042
juansubas <juansubas@gmail.com>
2024-02-13 12:23:33 -0500

    Update

--- components/About.tsx
+++ components/About.tsx
@@ -14,7 +14,7 @@ const About = () => {
   return (
     <section className='mx-auto max-w-[1440px] px-6 lg:px-20 3xl:px-0 gap-20 py-10 pb-32 md:gap-28 lg:py-20 bg-[#f7f7f7]' id='about'>
       <div className='text-center text-[18px] font-[700] uppercase tracking-[1rem] text-blue-500 pb-20'>
-        Quienes somos?
+        ¿Quienes somos?
       </div>
       <div className='flex flex-col gap-20 md:gap-28 xl:flex-row'>
         <div className='flex-1 flex items-center justify-center flex-col m-auto'>
